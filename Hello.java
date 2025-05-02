14a0aa9d529218d57eb07b0970fac8cbf683ff25
86006 <2021wb86006@wilp.bits-pilani.ac.in>
2025-05-02 12:28:01 +0530

    v2.0:Version Increased

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello from Java branch!");
+        System.out.println("Hello from Java branch!, Updating the version");
     }
 }
